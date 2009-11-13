923e435d8bc9f0c6a932384c259b8f8d9e760d76
thebigkahoona@gmail.com <thebigkahoona@gmail.com@44831380-b9ba-11de-8be4-e1425cda3908>
2009-11-13 23:22:20 +0000

    Logger is pretty much done. now Supports CSV format via the CsvWriter class. can be extended to other formats by implementing the DataWriter interface (e.g. XmlWriter). Also added the SimLabels enum and the SimEvents enum as descriptive enums. can be changed as we like later.
    Take a try and run the Log test case or the CsvWriter test case. 
    both create new CSV files under the new "logs" folder.

--- OLSRv2/src/log/ILog.java
+++ OLSRv2/src/log/ILog.java
@@ -1,5 +1,26 @@
+/**
+ * OLSRv2
+ * 
+ * Team members: Assaf Israel, Eli Nazarov, Asi Bross
+ *
+ * File: ILog.java
+ * Author: Assaf
+ * Date: 13/11/2009
+ *
+ */
 package log;
 
-public interface ILog {
-
+/**
+ * @author Assaf
+ * This is a default interface, used only by the Log class.
+ */
+interface ILog {
+	
+	/*
+	 * The log file naming convention is <fileName>_<date>.<fileExtension>
+	 */
+	public static final String fileName = "sim";
+	public static final String defaultFileExtension = "log";
+	
+	public static final String basePath = "logs/";
 }
