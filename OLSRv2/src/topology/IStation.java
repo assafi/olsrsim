76d55d5dcf674017a0590f6369ef7f9068e674ad
asibross <asibross@44831380-b9ba-11de-8be4-e1425cda3908>
2009-11-15 15:27:28 +0000

    Initial interface of a station class

---
+++ OLSRv2/src/topology/IStation.java
@@ -0,0 +1,38 @@
+/**
+ * OLSRv2
+ * 
+ * Team members: Assaf Israel, Eli Nazarov, Asi Bross
+ *
+ * File: IStation.java
+ * Author: Asi
+ * Date: 15/11/2009
+ *
+ */
+package topology;
+
+import java.awt.Point;
+
+import protocol.IOLSRv2Protocol;
+
+/**
+ * @author Asi
+ *
+ */
+public interface IStation {
+
+	/**
+	 * @return  Unique identification of a station
+	 */
+	String getStationID();
+	
+	/**
+	 * @return  Physical location of the station
+	 */
+	Point getStationLocation();
+	
+	/**
+	 * @return  OLSRv2 protocol layer of the station as defined in IOLSRv2Protocol interface
+	 */
+	IOLSRv2Protocol getOLSRv2Protocol();
+	
+}
