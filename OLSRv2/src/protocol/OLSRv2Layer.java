9cbfc3cb514597b637336b1a78f0528a476df6cf
eli.nazarov@gmail.com <eli.nazarov@gmail.com@44831380-b9ba-11de-8be4-e1425cda3908>
2009-12-19 10:46:24 +0000

    process of the TC message

--- OLSRv2/src/protocol/OLSRv2Layer.java
+++ OLSRv2/src/protocol/OLSRv2Layer.java
@@ -10,13 +10,17 @@
  */
 package protocol;
 
+import java.awt.TrayIcon.MessageType;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 import protocol.InformationBases.LocalInformationBase;
 import protocol.InformationBases.NeighborInformationBase;
 import protocol.InformationBases.NeighborProperty;
 import protocol.InformationBases.ReceivedMessageInformationBase;
+import protocol.InformationBases.ReceivedSetData;
 import protocol.InformationBases.TopologyCommonData;
 import protocol.InformationBases.TopologyInformationBase;
 import protocol.InformationBases.TopologySetData;
@@ -64,12 +68,20 @@ public class OLSRv2Layer implements IOLSRv2Layer {
 	private void updateRoutingSet(){
 		//TODO implement
 	}
+
+	/**
+	 * This method should send the TC message it received to all it's MPRs
+	 */
+	private void floodTCMsg(TCMessage tcMsg){
+		//TODO implement
+	}
 	
 	/* (non-Javadoc)
 	 * @see protocol.IOLSRv2Layer#receiveTCMessage(events.TCMessage)
 	 */
 	@Override
 	public void receiveTCMessage(TCMessage tcMsg) throws ProtocolException {
+		
 		/* We should update the following bases
 		 * 1. Update bases according to the TC message: (see OLSR rfc section 12.3)
 		 *  1.1. Populating the Advertising Remote Router Set - advertisingRemoteRouterSet in Topology Base
@@ -88,7 +100,33 @@ public class OLSRv2Layer implements IOLSRv2Layer {
 		 * 
 		 */
 		
+		
 		String src = tcMsg.getSource();
+		
+		/*
+		 * First we should check if the message was already processed 
+		 */
+		
+		if (receivedMsgInfo.getReceivedSet().containsKey(src)){
+			// each address is mapped to a list of messages it sent
+			ArrayList<ReceivedSetData> receviedMsgList = receivedMsgInfo.getReceivedSet().values().iterator().next();
+			for (ReceivedSetData receivedMsg : receviedMsgList) {
+				if (MessegeTypes.TC == receivedMsg.getType() && receivedMsg.getMsgHashCode() == tcMsg.hashCode()){
+					// if we found that the massage was already received we shouldn't process it
+					return;
+				}
+			}
+		}
+		else{ // Add the TC message to the Received TC messages base
+			
+			// add new entry for the originator the Receive set
+			// because this is the first time we receive a message from
+			// this originator we must allocate new receive message list.
+			ArrayList<ReceivedSetData> msgList = new ArrayList<ReceivedSetData>();
+			msgList.add(new ReceivedSetData(MessegeTypes.TC, tcMsg.hashCode(), tcMsg.getTime() + ProtocolDefinitions.EntryValidPeriod));
+			
+			receivedMsgInfo.getReceivedSet().put(src, msgList);
+		}
 		
 		// 1.1 Populating the Advertising Remote Router Set
 		if (!topologyInfo.getAdvertisingRemoteRouterSet().containsKey(src)){
@@ -125,9 +163,39 @@ public class OLSRv2Layer implements IOLSRv2Layer {
 		// 2. Update the routingSet in Topology Base
 		updateRoutingSet();
 		
-		// 3. Forward the tcMsg to all my MPRs
-		
-		// Add the TC message to the Received TC messages base
+		// 3. Forward the tcMsg to all my MPRs unless it was already forwarded
+		if (!receivedMsgInfo.getForwardSet().containsKey(src)){
+			floodTCMsg(tcMsg);// flood message
+			
+			// add new entry for the originator the Forward set
+			// because this is the first time we forward a message from
+			// this originator we must allocate new message list.
+			ArrayList<ReceivedSetData> msgList = new ArrayList<ReceivedSetData>();
+			msgList.add(new ReceivedSetData(MessegeTypes.TC, tcMsg.hashCode(), tcMsg.getTime() + ProtocolDefinitions.EntryValidPeriod));
+			
+			receivedMsgInfo.getForwardSet().put(src, msgList);
+		}
+		else{
+			ArrayList<ReceivedSetData> forwardMsgList = receivedMsgInfo.getForwardSet().values().iterator().next();
+			boolean floodMsg = true;
+			for (ReceivedSetData receivedMsg : forwardMsgList) {
+				if (MessegeTypes.TC == receivedMsg.getType() && receivedMsg.getMsgHashCode() == tcMsg.hashCode()){
+					// if we found that the massage was already forwarded we shouldn't flood it
+					floodMsg = false;
+					break;
+				}
+			}
+			// if the hash-code of the massage is not in the forward 
+			// list of the originator we must flood the TC message,
+			// and add it to the list.
+			if (floodMsg){
+				floodTCMsg(tcMsg);// flood message
+				
+				// add to the list
+				forwardMsgList.add(new ReceivedSetData(MessegeTypes.TC, tcMsg.hashCode(), tcMsg.getTime() + ProtocolDefinitions.EntryValidPeriod));
+			}
+			
+		}	
 	}
 
 	/* (non-Javadoc)
